ccb5d159a678e8e52637083d4e51bb1fa07c328c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-11-26 15:01:44 -0600

    Se quito el boton de registrar de la tabla

--- src/pages/Ftn/FtnIndex.tsx
+++ src/pages/Ftn/FtnIndex.tsx
@@ -142,13 +142,6 @@ export const FtnIndex = () => {
                 </svg>
                 <h3 className="mt-2 text-sm font-medium text-gray-900">No se encontraron registros</h3>
                 <p className="mt-1 text-sm text-gray-500">Intenta ajustar los filtros de búsqueda.</p>
-                <button
-                    onClick={handleCreate}
-                    className="mt-4 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg 
-                    font-medium transition-colors duration-200 hover:cursor-pointer"
-                >
-                    Registrar nueva entrada
-                </button>
             </div>
         );
     };
